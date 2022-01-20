375be28a2861d531d520810c55520abd84176f11
Stuart Sy <stuart.sy@focusglobalinc.com>
2022-01-20 20:31:12 +0800

    update fish config and add more fish functions

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
-# We only want to do our user setup when a new actual terminal is opened, 
-# not when fish is called for sub functions
 if status is-interactive
-	fish_logo
-	print_waves -l 36
-	set --universal fish_greeting
-	fortune
-	echo ""
+    # Commands to run in interactive sessions can go here
+    set -g theme_nerd_fonts yes
+    set -g theme_display_git_default_branch yes
+    set -g theme_color_scheme solarized-dark
+    echo ""
 end
+fish_add_path /usr/local/opt/python@3.8/bin
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
